68ad86208d4b16329888f0f3f9a63cd87937821f
Van Phan <VanPhan514@gmail.com>
2015-07-14 17:38:26 -0400

    added js logic for images

--- js/apicontroller.js
+++ js/apicontroller.js
@@ -44,26 +44,110 @@ app.controller('apiController', ['$http', '$scope', function($http, $scope){
 			});
 			link = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + userid + '/recent?api_key=d4095fd1-ec30-4403-8db7-fdacebef9b17';
 			$http.get(link).success(function(data){
-				$scope.games = getSummonerIcons(data);
-			});
+				for (var i = 0; i < data.games.length; i++) {
+					switch(data.games[i].spell1) {
+						case 1:
+							data.games[i].spell1 = "SummonerBoost";
+						break;
+						case 2:
+							data.games[i].spell1 = "SummonerClairvoyance";
+						break;
+						case 3:
+							data.games[i].spell1 = "SummonerExhaust";
+						break;
+						case 4:
+							data.games[i].spell1 = "SummonerFlash";
+						break;
+						case 6:
+							data.games[i].spell1 = "SummonerHaste";
+						break;
+						case 7:
+							data.games[i].spell1 = "SummonerHeal";
+						break;
+						case 11:
+							data.games[i].spell1 = "SummonerSmite";
+						break;
+						case 12:
+							data.games[i].spell1 = "SummonerTeleport";
+						break;
+						case 13:
+							data.games[i].spell1 = "SummonerMana";
+						break;
+						case 14:
+							data.games[i].spell1 = "SummonerDot";
+						break;
+						case 17:
+							data.games[i].spell1 = "SummonerOdinGarrison";
+						break;
+						case 21:
+							data.games[i].spell1 = "SummonerBarrier";
+						break;
+						case 30:
+							data.games[i].spell1 = "SummonerPoroRecall";
+						break;
+						case 31:
+							data.games[i].spell1 = "SummonerPoroThrow";
+						break;
+						case 32:
+							data.games[i].spell1 = "SummonerSnowball";
+						break;
+						default:
+							break;
+					};
 
-		};
-
-		var getSummonerIcons = function (dataFromMatches) {
-			var link = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/summoner-spell?dataById=true&api_key=d4095fd1-ec30-4403-8db7-fdacebef9b17';
-			var iconApi;
-			$http.get(link).success(function(data){ 
-				iconApi = data;
-				angular.forEach(dataFromMatches, function(val, key) {
-				for (var i = 0; i < val.length; i++) {
-					switch (val[i].spell1) {
-						case 1: 
-
+					switch(data.games[i].spell2) {
+						case 1:
+							data.games[i].spell2 = "SummonerBoost";
+						break;
+						case 2:
+							data.games[i].spell2 = "SummonerClairvoyance";
+						break;
+						case 3:
+							data.games[i].spell2 = "SummonerExhaust";
+						break;
+						case 4:
+							data.games[i].spell2 = "SummonerFlash";
+						break;
+						case 6:
+							data.games[i].spell2 = "SummonerHaste";
+						break;
+						case 7:
+							data.games[i].spell2 = "SummonerHeal";
+						break;
+						case 11:
+							data.games[i].spell2 = "SummonerSmite";
+						break;
+						case 12:
+							data.games[i].spell2 = "SummonerTeleport";
+						break;
+						case 13:
+							data.games[i].spell2 = "SummonerMana";
+						break;
+						case 14:
+							data.games[i].spell2 = "SummonerDot";
+						break;
+						case 17:
+							data.games[i].spell2 = "SummonerOdinGarrison";
+						break;
+						case 21:
+							data.games[i].spell2 = "SummonerBarrier";
+						break;
+						case 30:
+							data.games[i].spell2 = "SummonerPoroRecall";
+						break;
+						case 31:
+							data.games[i].spell2 = "SummonerPoroThrow";
+						break;
+						case 32:
+							data.games[i].spell2 = "SummonerSnowball";
+						break;
+						default:
+							break;
 					};
 				};
-				// iconApi.data[1].name
+				$scope.games = data;
 			});
-			});
+
 		};
-		
+
 	}]);
